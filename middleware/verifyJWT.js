607f0623c56a470846fc1aa387f712880b565dd9
Michelle Williams <michellewilliams@Michelles-MacBook-Pro.local>
2023-12-16 20:44:04 -0700

    add/connect verify jwt logic

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,21 +1,24 @@
-import jwt from "jsonwebtoken";
+// @ts-nocheck
+import jwt from 'jsonwebtoken';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const verifyJWT = (req, res, next) => {
   const authHeader = req.headers.authorization || req.headers.Authorization;
 
-  if (!authHeader?.startsWith("Bearer ")) {
-    return res.sendStatus(401); //Unauthorized
+  if (!authHeader?.startsWith('Bearer ')) {
+    return res.sendStatus(401); // Unauthorized
   }
 
-  const token = authHeader.split(" ")[1];
+  const token = authHeader.split(' ')[1];
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      return res.sendStatus(403); //invalid token - forbidden
+      return res.sendStatus(403); // invalid token - forbidden
     }
 
-    req.user = decoded.UserInfo.username;
-    req.roles = decoded.UserInfo.roles;
+    req.user = decoded.user;
     next();
   });
 };
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -9,16 +9,19 @@ import {
 import { validate } from '../middleware/validators/validator.js';
 import { createUserValidationRules } from '../middleware/validators/userValidation.js';
 import verifyRoles from '../middleware/verifyRoles.js';
+import verifyJWT from '../middleware/verifyJWT.js';
 
 const router = express.Router();
 
 router
   .route('/')
-  .get(getUsers)
+  .get(verifyJWT, getUsers)
   .post(createUserValidationRules(), validate, createUser);
 
-router.route('/:id').get(getUser).put(updateUser).delete(deleteUser);
+router
+  .route('/:id')
+  .get(verifyJWT, getUser)
+  .put(verifyJWT, updateUser)
+  .delete(verifyJWT, deleteUser);
 
 export default router;
-
-// {"firstName": "MatMichelle", "lastName": "Joe", "email": "matmic@gmail.com", "password": "test123"}
