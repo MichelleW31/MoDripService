817d221e295589e379ae8f4e802fb5b48c9a90cd
Michelle Williams <michellewilliams@Michelles-MacBook-Pro.local>
2024-11-20 21:26:05 -0700

    add logging

--- middleware/convertSensorReadings.js
+++ middleware/convertSensorReadings.js
@@ -1,11 +1,14 @@
+import logger from '../config/logger.js';
+
 const getMoisturePercentage = (moistureValue) => {
+  logger.info('moisture value', moistureValue);
   // Get Range
   const minValue = 1400;
   const maxValue = 2900;
   const moistureRange = maxValue - minValue;
 
   // Calculate percentage
-  const moisturePercentage = ((maxValue - moistureValue) / moistureRange) * 100;
+  const moisturePercentage = (maxValue - moistureValue / moistureRange) * 100;
 
   return moisturePercentage;
 };
