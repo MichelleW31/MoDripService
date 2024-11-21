51884e7e4d6bbce000d959974199c7a3ba30965b
Michelle Williams <michellewilliams@Michelles-MacBook-Pro.local>
2024-11-20 21:32:31 -0700

    update percentage logic

--- middleware/convertSensorReadings.js
+++ middleware/convertSensorReadings.js
@@ -2,13 +2,21 @@ import logger from '../config/logger.js';
 
 const getMoisturePercentage = (moistureValue) => {
   logger.info('moisture value', moistureValue);
+
   // Get Range
   const minValue = 1400;
   const maxValue = 2900;
   const moistureRange = maxValue - minValue;
+  let moisturePercentage = 0;
 
   // Calculate percentage
-  const moisturePercentage = (maxValue - moistureValue / moistureRange) * 100;
+  if (moistureValue > 2900) {
+    moisturePercentage = 0;
+  } else if (moistureValue < 1400) {
+    moisturePercentage = 100;
+  } else {
+    moisturePercentage = ((maxValue - moistureValue) / moistureRange) * 100;
+  }
 
   return moisturePercentage;
 };
