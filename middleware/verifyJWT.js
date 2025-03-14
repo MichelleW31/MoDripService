1e3b320071159055d4d0eeb2cc990044bd8c01ab
Michelle Williams <michellewilliams@Michelles-MacBook-Pro.local>
2025-03-13 20:31:24 -0700

    add token verification logic

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -2,21 +2,29 @@
 import jwt from 'jsonwebtoken';
 import * as dotenv from 'dotenv';
 import logger from '../config/logger.js';
+import { admin } from '../FirebaseConfig.js';
 
 dotenv.config();
 
-const verifyJWT = (req, res, next) => {
+const verifyJWT = async (req, res, next) => {
   const authHeader = req.headers.authorization || req.headers.Authorization;
 
   if (!authHeader?.startsWith('Bearer ')) {
     return res.sendStatus(401); // Unauthorized
   }
 
-
-
   const token = authHeader.split(' ')[1];
 
-  logger.info(`AUTH TOKEN ${token}`)
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(token);
+
+    logger.info(`Auth Token verified ${decodedToken}`);
+  } catch (error) {
+    logger.error(`Error ${err}`);
+    logger.info(`Access token: ${authHeader}`);
+
+    return res.sendStatus(403); // invalid token - forbidden
+  }
 
   // jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
   //   if (err) {
