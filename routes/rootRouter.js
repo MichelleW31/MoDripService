e3032b80f7d395deed09ec420dd4d5cc938ba549
Michelle Williams <michellewilliams@Michelles-MacBook-Pro.local>
2025-03-20 22:06:39 -0700

    remove refresh token logic

--- routes/rootRouter.js
+++ routes/rootRouter.js
@@ -2,14 +2,12 @@ import { Router } from 'express';
 import userRoutes from './usersRoute.js';
 import modsRoute from './modsRoute.js';
 import targetDataRoutes from './targetDataRoute.js';
-import refreshTokenRoute from './refreshTokenRoute.js';
 
 let rootRouter = Router();
 
 rootRouter.use('/users', userRoutes);
 rootRouter.use('/mods', modsRoute);
 rootRouter.use('/target_data', targetDataRoutes);
-rootRouter.use('/refresh_token', refreshTokenRoute);
 
 rootRouter.get('/', (req, res) => {
   res.status(200).send('Welcome to MoDrip');
