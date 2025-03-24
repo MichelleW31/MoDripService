1dfe53808a8fa281b192826f36ce0208c648cdd3
Michelle Williams <michellewilliams@Michelles-MacBook-Pro.local>
2025-03-23 20:05:02 -0700

    update backend code

--- middleware/validators/validator.js
+++ middleware/validators/validator.js
@@ -1,15 +1,15 @@
 import { validationResult } from 'express-validator';
 import logger from '../../config/logger.js';
 
-export const validate = (req, res, next) => {
-  const errors = validationResult(req);
+export const validate = async (req, res, next) => {
+  const errors = await validationResult(req);
 
   if (errors.isEmpty()) {
     return next();
   }
   const errorsArray = [];
   errors.array().map((err) => errorsArray.push({ [err.param]: err.msg }));
-  logger.info(errorsArray);
+  logger.error(errorsArray);
 
   // Return bad request
   return res.status(400).json({
